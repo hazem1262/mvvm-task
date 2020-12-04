9b9ee496b5ea90846caa5746b4f3f3ac157e4511
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-12-04 21:41:28 +0200

    [add] add sign up form validation

--- app/src/main/java/com/example/mvvmstarterproject/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/example/mvvmstarterproject/ui/signup/SignUpFragment.kt
@@ -2,14 +2,21 @@ package com.example.mvvmstarterproject.ui.signup
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.mvvmstarterproject.R
 import com.example.mvvmstarterproject.base.BaseFragment
 import com.opensooq.supernova.gligar.GligarPicker
 import kotlinx.android.synthetic.main.sign_up_fragment.*
+
 
 class SignUpFragment : BaseFragment<SignUpViewModel>() {
 
@@ -23,6 +30,47 @@ class SignUpFragment : BaseFragment<SignUpViewModel>() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         handleImageSelection()
+        handleSignUPButton()
+        handleBackButton()
+        addFormValidation()
+    }
+
+    private fun addFormValidation() {
+        nameField.addTextChangedListener(object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                validateName()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+        emailField.addTextChangedListener(object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                validateEmail()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+        passwordField.addTextChangedListener(object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                validatePassword()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    private fun handleBackButton() {
+        ic_back_button.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun handleSignUPButton() {
+        signUpBtn.setOnClickListener {
+            if (validateName() && validateEmail() && validatePassword() && validateImageSelection()){
+                findNavController().navigate(R.id.action_signUpFragment_to_sendMessageFragment)
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -52,6 +100,70 @@ class SignUpFragment : BaseFragment<SignUpViewModel>() {
                 .cameraDirect(false)
                 .show()
         }
+    }
+
+    private fun validateEmail(): Boolean {
+        if (emailField.text.toString().trim().isEmpty()) {
+            email_ET.error = getString(R.string.empty_email_validation)
+            requestFocus(emailField)
+            return false
+        } else {
+            val emailValue: String = emailField.text.toString()
+            val isValid =
+                Patterns.EMAIL_ADDRESS.matcher(emailValue).matches()
+            if (!isValid) {
+                email_ET.error = getString(R.string.not_valid_email_msg)
+                requestFocus(emailField)
+                return false
+            } else {
+                email_ET.isErrorEnabled = false
+            }
+        }
+        return true
+    }
+
+    private fun validatePassword(): Boolean {
+        when {
+            passwordField.text.toString().trim().isEmpty() -> {
+                password_ET.error = getString(R.string.empty_password_validation)
+                requestFocus(passwordField)
+                return false
+            }
+            passwordField.text.toString().length < 6 -> {
+                password_ET.error = getString(R.string.password_validation)
+                requestFocus(passwordField)
+                return false
+            }
+            else -> {
+                password_ET.isErrorEnabled = false
+            }
+        }
+        return true
+    }
+
+    private fun validateName(): Boolean {
+        if (nameField.text.toString().trim().isEmpty()) {
+            full_name_ET.error = getString(R.string.empty_name_validation)
+            requestFocus(nameField)
+            return false
+        } else{
+            full_name_ET.isErrorEnabled = false
+        }
+        return true
+    }
+
+    private fun requestFocus(view: View) {
+        if (view.requestFocus()) {
+            activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        }
+    }
+
+    private fun validateImageSelection(): Boolean{
+        if (viewModel.selectedImagePath.isBlank()){
+            Toast.makeText(requireContext(), getString(R.string.select_image_validation), Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
     }
 }
 
--- app/src/main/java/com/example/mvvmstarterproject/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/mvvmstarterproject/ui/signup/SignUpViewModel.kt
@@ -4,5 +4,9 @@ import com.example.mvvmstarterproject.base.BaseViewModel
 import javax.inject.Inject
 
 class SignUpViewModel @Inject constructor(): BaseViewModel() {
+    fun isValidForm(): Boolean {
+        return true
+    }
+
     var selectedImagePath:String = ""
 }
